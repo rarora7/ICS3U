b456c0458221c3dddfcd60b5c527af2de49744e9
rarora <rarora@rarorasp3.bayviewglen.ca>
2016-09-15 11:29:59 -0400

    Day five work

---
+++ Unit One/src/com/bayviewglen/dayfive/DayFiveHomeworkOne.java
@@ -0,0 +1,28 @@
+package com.bayviewglen.dayfive;
+
+import java.util.Scanner;
+
+public class DayFiveHomeworkOne {
+
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner (System.in);
+		
+		System.out.println("Please enter a number");
+		
+		double y = keyboard.nextDouble();
+		
+		System.out.println(y);
+		
+		System.out.println(y*y);
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Unit One/src/com/bayviewglen/dayfive/DayFiveHomeworkTwo.java
@@ -0,0 +1,16 @@
+package com.bayviewglen.dayfive;
+
+import java.util.Scanner;
+
+public class DayFiveHomeworkTwo {
+
+	public static void main(String[] args) {
+	
+Scanner keyboard = new Scanner (System.in);
+		
+		System.out.println("Please enter a number");
+		x = keyboard.nextInt(); 
+
+	}
+
+}
